550229cae79420a1d9b34551e9d6ce8b773f63cc
Lorenzo Beretta <vc.net.loreb@gmail.com>
2020-12-18 16:57:39 +0100

    day 16 in perl^Wpython
    While things have been a bit weird and so I often do the advent when I'm tired,
    so the perl version is much more messy than it needs to be, I got to the point where part2
    didn't work for some reason and redid it (with hard-coded values) in python3.
    
    It was **so** much faster&easier to get it right!
    Most of the bugs were brainfarts copypasting & replacing text from the input...
    Possible explanations:
    1. I'm more experienced with python than perl - no
    2. Perl becomes great when it becomes second nature but it sucks if you only use it for short silly scripts
    3. My brain is more python-shaped than perl-shaped
    4. Perl just sucks compared to python
    
    Cursing aside, I believe it's John Michael Dorian.

---
+++ perm16.pl
@@ -0,0 +1,227 @@
+#! /usr/bin/perl
+# language picked by /dev/urandom; TODO maybe zig? (not forth!)
+use strict;
+use warnings;
+use diagnostics;
+
+die "usage: $0 ticketfile" unless $#ARGV == 0;
+open(my $fh, '<', $ARGV[0]) or die "open ticket: $!";
+
+my $dbg = \&CORE::warn;
+$dbg = sub {} if defined $ENV{'NDEBUG'};
+
+# comma separated numbers => array reference with those numbers.
+sub parse_ticket($) {
+    my ($line) = @_;
+    chomp $line;
+    my @fields;
+    while (length($line) > 0) {
+        die $line unless $line =~ /([0-9]+),?(.*)/;
+        # "It doesn't matter which position corresponds to which field"
+        $dbg->("$1 in line $line");
+        push @fields, $1;
+        $line = $2;
+    }
+    $dbg->("ticket = ", join(' ', @fields));
+    return \@fields;
+}
+
+# 1. rules, eg "class: 1-3 or 5-7"
+# rule{'class'} = (1,3) means class is 1,2,or 3
+# except we need an array of arrays (inclusive or) => array of references
+my %rule;
+while (<$fh>) {
+    chomp;
+    my $line = $_;
+    $dbg->("line($line)");
+    # "departure station:" => can't use eg (\w+)
+    last unless $line =~ /^(.+): *(.*)/;
+    my $field = $1;
+    $dbg->("field($field)");
+    die "$field appears twice in line($line)" if defined $rule{$field};
+    my @all;
+    my $todo = $2;
+    while (length($todo) > 0) {
+        die "$todo($line)" unless $todo =~ /([0-9]+)\-([0-9]+) *(.*)/;
+        my ($min,$max) = ($1,$2);
+        die "min($min),max($max) in line $line" unless $max >= $min;
+        $dbg->("min=$min max=$max");
+        push @all, [$min,$max];
+        $todo = $3;
+        $todo =~ s/^or +//;
+    }
+    die "empty rules for $field?" unless $#all >= 0;
+    $rule{$field} = \@all;
+}
+
+# 2.: "your ticket:" (empty line eaten in the previous loop)
+my $ticket;
+while (<$fh>) {
+    die $_ unless $_ =~ /your ticket:/;
+    my $line = <$fh>;
+    die "??" unless defined $line;
+    $ticket = parse_ticket($line);
+    last;
+}
+die "no ticket?" unless defined $ticket;
+
+# 3: empty line, then "nearby tickets:", then list of tickets.
+my $line_empty = <$fh>;
+my $line_nearby = <$fh>;
+die "wanted nearby tickets: $line_nearby" unless $line_nearby eq "nearby tickets:\n";
+my @tickets;
+while (<$fh>) {
+    push @tickets, parse_ticket($_);
+}
+
+# part1: find tickets that contain ONE fields that CANNOT be any valid value
+# (ie, out of range for all rules) => sum those invalid fields
+
+sub valid_field($) {
+    my ($value) = (@_);
+    foreach my $field(keys %rule) {
+        my $ref = $rule{$field};
+        foreach my $minmaxref(@$ref) {
+            my @minmax = @$minmaxref;
+            my ($min,$max) = @minmax;
+            $dbg->("$field: $min <= $value <= $max?");
+            return 1 if ($min <= $value && $value <= $max);
+        }
+    }
+    $dbg->("BAD ticket field ($value)");
+    return 0;
+}
+# return field value, undef if ok
+sub invalid_field_in_ticket($) {
+    my ($ticket) = @_;
+    my $retval;
+    foreach my $field(@$ticket) {
+        next if valid_field($field);
+        die "invalid $retval and $field" if defined $retval;
+        $dbg->("bad field: $field");
+        $retval = $field;
+    }
+    return $retval;
+}
+
+sub part1() {
+    my $total = 0;
+    foreach my $nearby(@tickets) {
+        my $bad = invalid_field_in_ticket($nearby);
+        next unless defined $bad;
+        $dbg->("$total += $bad");
+        $total += $bad;
+    }
+    print "total = $total\n";
+}
+part1() if 0; # getopt or whatever
+
+# --- Part Two ---
+# Now that you've identified which tickets contain invalid values, discard those tickets entirely.
+# Use the remaining valid tickets to determine which field is which...
+
+sub only_valid_tickets {
+    my @validtickets;
+    foreach my $nearby(@tickets) {
+        next if invalid_field_in_ticket($nearby);
+        push @validtickets, $nearby;
+    }
+    return @validtickets;
+}
+@tickets = only_valid_tickets(@tickets);
+
+sub arraylen {
+    return 1 + $#_; # I always make this mistake
+}
+
+# eg valid_positions[row] = (0,1,2) => test until only ONE valid position remains.
+my %valid_positions; # INDEX into array!
+my $nfields = arraylen(keys %rule);
+foreach my $field(keys %rule) {
+    my @a = 0..$nfields;
+    pop @a; # 0 .. n-1
+    $valid_positions{$field} = \@a;
+}
+
+sub mark_invalid($$) {
+    my ($arrayref, $i) = @_;
+    die "??" unless defined $arrayref->[$i];
+    $arrayref->[$i] = -666;
+}
+
+sub marked_invalid($$) {
+    my ($arrayref, $i) = @_;
+    die "??" unless defined $arrayref->[$i];
+    return $arrayref->[$i] < 0;
+}
+
+sub value_follows_rule($$) {
+    my ($value, $ruleref) = @_;
+    foreach my $minmaxref(@$ruleref) {
+        my @minmax = @$minmaxref;
+        my ($min,$max) = @minmax;
+        #$dbg->("$min <= $value <= $max?");
+        return 1 if ($min <= $value && $value <= $max);
+    }
+    return 0;
+}
+
+# pruning out invalid fields is not enough for obvious reasons,
+# even in the test input => try all permutations!
+# XXX no, it turns out there are 30 fields => 30 factorial is > 2^100...
+
+sub invalid_permutation {
+    my $ticketsref = shift or die "!";
+    my @perm = @_;
+    foreach my $ticket(@$ticketsref) {
+        my $i = 0;
+        foreach my $field(@perm) {
+            my $r = $rule{$field};
+            my $value = $ticket->[$i];
+            $i++;
+            next if value_follows_rule($value, $r);
+            return 1;
+        }
+    }
+    return 0;
+}
+# I'm NOT doing this by hand...
+use List::Permutor;
+
+my $perm = new List::Permutor (keys %rule);
+my $found = 0;
+my @validperm;
+while (my @set = $perm->next) {
+    $dbg->("permutation: ", join(' ', @set));
+    next if invalid_permutation(\@tickets, @set);
+    $found = 1;
+    @validperm = @set;
+    last;
+}
+die "nothing valid?" unless $found;
+$dbg->("valid permutation: ", join(' ', @validperm));
+my %fieldidx;
+for (my $i = 0; $i < arraylen(@validperm); $i++) {
+    $fieldidx{$validperm[$i]} = $i;
+}
+
+# Once you work out which field is which, look for the six fields on your ticket that start with the word departure.
+# What do you get if you multiply those six values together?
+# my ticket is simply called "ticket" (because "my $ticket")
+my $myticket = $ticket;
+my $mul = 1;
+foreach my $field(keys %rule) {
+    next unless $field =~ /^departure/;
+    $dbg->("field($field)");
+    my $i = $fieldidx{$field};
+    my $value = $myticket->[$i];
+    $dbg->("mult by $value");
+    $mul *= $value;
+}
+print "we obtain $mul";
+
+
+
+
+...;
+
